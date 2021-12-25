402e00d785e52ed1203227de085cdc3f4fc7b4c1
doanduccuong <cuongbkk63@gmail.com>
2021-12-25 22:38:32 +0700

    update

--- lib/ui/sign_in_screen/sign_in_page.dart
+++ lib/ui/sign_in_screen/sign_in_page.dart
@@ -22,7 +22,7 @@ class SignInPage extends StatelessWidget {
             ),
             const Center(
               child: Text(
-                'Welcome Back Back!',
+                'Welcome Back Back back!',
                 style: TextStyle(
                   fontFamily: 'Poppins',
                   fontWeight: FontWeight.w600,
