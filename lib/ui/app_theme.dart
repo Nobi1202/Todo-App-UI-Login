c2a4fe1e305bab1fd6f4272622151ff504ef6a1b
Cuong <cuong12c12000@gmail.com>
2021-12-25 22:03:04 +0700

    update

--- lib/ui/app_theme.dart
+++ lib/ui/app_theme.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:todo_app/configs/images.dart';
 class AppTheme extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
     return Stack(
